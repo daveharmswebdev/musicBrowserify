825aa91e027f8c1b916b98199813fbe94f825374
Dave Harms <daveharmswebdev@gmail.com>
2016-06-16 16:19:33 -0500

    able to load songs, will now begin to render

--- js/index.js
+++ js/index.js
@@ -1,3 +1,3 @@
 var loadSongs = require('../js/loadSongs.js');
 
-console.log(loadSongs());
+loadSongs.read();
--- js/loadSongs.js
+++ js/loadSongs.js
@@ -1,5 +1,15 @@
-var test = function() {
-	return 'test';
+var $ = require('jQuery');
+var render = require('../js/render.js');
+var load = {};
+
+load.songs = null;
+load.read = function() {
+	$.ajax({
+		url: 'data/songs.json'
+	}).done(function(response) {
+		render.stageJsonResponse(response.songs);
+	});
 };
 
-module.exports = test;
+
+module.exports = load;
