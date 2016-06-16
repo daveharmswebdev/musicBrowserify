d56822e1ab4f45dd69ba32deee2f7ce321ca307b
Dave Harms <daveharmswebdev@gmail.com>
2016-06-16 16:49:10 -0500

    installed handlebars and tested a template

--- js/render.js
+++ js/render.js
@@ -1,11 +1,12 @@
 var $ = require('jQuery');
+var template = require('../template.hbs');
 var render = {};
 
 render.stageJsonResponse = function(songs) {
 	songs.forEach((song) => this.renderSong(song));
 };
 render.renderSong = function(song) {
-	$('#list__post').append(`<p>${song.title}</p>`);
+	$('#list__post').append(template({name: song.title}));
 };
 
 module.exports = render;
