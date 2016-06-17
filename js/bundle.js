7e435666169a7fdae8919b94acd41faa377f0bc1
Dave Harms <daveharmswebdev@gmail.com>
2016-06-16 22:37:58 -0500

    can load songs to songs array

--- js/bundle.js
+++ js/bundle.js
@@ -5,15 +5,20 @@ loadSongs.read();
 },{"../js/loadSongs.js":2}],2:[function(require,module,exports){
 var $ = require('jQuery');
 var render = require('../js/render.js');
+var songs = [];
 var load = {};
 
-load.songs = null;
 load.read = function() {
 	$.ajax({
 		url: 'data/songs.json'
 	}).done(function(response) {
+		response.songs.forEach(load.setSongs);
+		console.log(songs);
 		render.stageJsonResponse(response.songs);
 	});
+};
+load.setSongs = function(song) {
+	songs.push(song);
 };
 
 
--- js/loadSongs.js
+++ js/loadSongs.js
@@ -1,14 +1,19 @@
 var $ = require('jQuery');
 var render = require('../js/render.js');
+var songs = [];
 var load = {};
 
-load.songs = null;
 load.read = function() {
 	$.ajax({
 		url: 'data/songs.json'
 	}).done(function(response) {
+		response.songs.forEach(load.setSongs);
+		console.log(songs);
 		render.stageJsonResponse(response.songs);
 	});
+};
+load.setSongs = function(song) {
+	songs.push(song);
 };
 
 
